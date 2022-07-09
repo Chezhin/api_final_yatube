7fc6f66169e19fada8d741f1caaff6204221d208
Руслан Чежин <4ezhin@gmail.com>
2022-07-09 21:02:11 +0300

    project

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -17,7 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (
-        permissions.IsAuthenticated,
+        permissions.IsAuthenticatedOrReadOnly,
         IsAuthorOrReadOnly
     )
     pagination_class = LimitOffsetPagination
@@ -35,17 +35,17 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (
-        permissions.IsAuthenticated,
+        permissions.IsAuthenticatedOrReadOnly,
         IsAuthorOrReadOnly
     )
 
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         queryset = post.comments.all()
         return queryset
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         serializer.save(author=self.request.user, post=post)
 
 
